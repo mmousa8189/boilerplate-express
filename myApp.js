451b0ef7887e8ef8ce6833e455a1fa6b016d87db
Mohamed Mousa <mohamedamousa00@gmail.com>
2024-02-28 16:09:28 +0000

    views

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ let express = require("express");
 let app = express();
 
 console.log("Hello World");
+const absolutePath = __dirname + '/views/index.html';
+
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
 module.exports = app;
