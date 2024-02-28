5bf4518dbee93b458213154d3f47e8bdd994ea0d
Mohamed Mousa <mohamedamousa00@gmail.com>
2024-02-28 16:15:15 +0000

    public

--- myApp.js
+++ myApp.js
@@ -5,7 +5,18 @@ console.log("Hello World");
 
 
 
+
+
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+//app.use("/public", express.static(__dirname + "/public"));
+
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+
 module.exports = app;
